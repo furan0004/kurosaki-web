9fa87241ca520c7ace04eea1f25da8474dfe9eeb
くろん <furan0004@gmail.com>
2022-08-07 07:41:39 +0900

    Update index.js

--- index.js
+++ index.js
@@ -13,18 +13,5 @@ function showPage(){
 
 
 (function(){
-	let href = location.href;
-	let ref = document.referrer;
-	
-	if(true){
-		let spl = ref.split("/"); 
-		if(spl[2] == "gloyah.net" && spl[4].split("?")[0] =="E0an"){
-			showPage();
-			alert("complete");
-		}else{
-			document.write("Wrong Referrer\n<a href='http://evassmat.com/E0an'>Go through the correct route</a>");
-		}
-	}else{
-		alert(href);
-	}
+	showPage();
 })();
