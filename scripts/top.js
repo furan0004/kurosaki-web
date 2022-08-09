e5e28c816a8f61aafeae5fd1ff302c3c0e923e2d
かずま <furan0004@gmail.com>
2022-08-09 11:59:03 +0900

    big fixes

--- scripts/top.js
+++ scripts/top.js
@@ -1,117 +1,170 @@
-const BASE_CLASS_NAME = "topbar";
-
-var urls = [
-    [
-        "Twitter Main",
-        "https://twitter.com/KuronKurosaki",
-        "res/images/twitter.svg",
-    ],
-    [
-        "Twitter Sub",
-        "https://twitter.com/kuron_nano",
-        "res/images/twitter.svg",
-    ],
-    [
-        "YouTube 1",
-        "https://www.youtube.com/channel/UCArGke6099LSxDdHbzJOFnQ",
-        "res/images/youtube.svg",
-    ],
-    [
-        "YouTube 2",
-        "https://www.youtube.com/channel/UCrjrRi8XRbQ03j_YlvVVKmA",
-        "res/images/youtube.svg",
-    ],
+var menuItems= [
+    {
+        text: "Twitter Main",
+        url: "https://twitter.com/KuronKurosaki",
+        icon: "res/images/twitter.svg",
+    },
+    {
+        text: "Twitter Sub",
+        url: "https://twitter.com/kuron_nano",
+        icon: "res/images/twitter.svg",
+    },
+    {
+        text: "YouTube 1",
+        url: "https://www.youtube.com/channel/UCArGke6099LSxDdHbzJOFnQ",
+        icon: "res/images/youtube.svg",
+    },
+    {
+        text: "YouTube 2",
+        url: "https://www.youtube.com/channel/UCrjrRi8XRbQ03j_YlvVVKmA",
+        icon: "res/images/youtube.svg",
+    },
 ];
 
+
 class TopBar {
-    #back;
-    #background = "url('res/images/flower5544.jpg')";
+    static DEFAULT_RESOURCES = {
+        BACKGROUND: "res/images/flower5544.jpg",
+        ICON: {
+            HOME: "res/images/home.svg",
+            TWITTER: "res/images/twitter.svg",
+            YOUTUBE: "res/images/youtube.svg",
+        },
+    };
+
+    static CLASS = {
+        BASE: "topbar",
+        BACK: "topbar_back",
+        COVER: "topbar_cover",
+
+        TOP_MENU: "topbar_top_menu",
+        HOME_BUTTON: "topbar_top_menu_home",
+
+        BLANK: "topbar_blank",
+        TITLEBAR: "topbar_title",
+
+        BOTTOM_MENU: "topbar_bottom_menu",
+        BOTTOM_MENU_BUTTON: "topbar_bottom_menu_button",
+        BOTTOM_MENU_BUTTON_LABEL: "topbar_bottom_menu_button_label",
+        BOTTOM_MENU_BUTTON_LABEL_TEXT: "topbar_bottom_menu_button_label_text",
+        BOTTOM_MENU_BUTTON_LABEL_ICON: "topbar_bottom_menu_button_label_icon",
+    };
+
+    #elements = {
+        back: null,
+        cover: null,
+        topMenu: null,
+        homeButton: null,
+        bottomMenu: null,
+        bottomMenuBtns: [],
+    };
+
+    #bottomMenuItems = [];
+    /*
+    item example:
+        {
+            text: "Google",
+            icon: ".../google.png",
+            url: "https://google.co.uk",
+        }
+    */
+
+
+    #background = `url('${TopBar.DEFAULT_RESOURCES.BACKGROUND}')`;
 
     #home;
-    #HOME_ICON = "res/images/home.svg";
+    #homeIcon = TopBar.DEFAULT_RESOURCES.ICON.HOME;
 
-    #title = "No Title";
+    #title;
 
     constructor(title){
+        this.#elements.back = document.createElement("div");
+        this.#elements.back.classList.add(TopBar.CLASS.BACK);
+
         this.renew();
-        if(title != null) this.#title = title;
+        this.#title = title || "No Title";
     }
 
     renew(){
-        if(this.#back != null) this.#back.remove();
-        this.#back = document.createElement("div");
-        this.#back.className = BASE_CLASS_NAME + "_back";
+        if(this.#elements.cover != null) this.#elements.cover.remove();
+        this.#elements.cover = document.createElement("div");
+        this.#elements.cover.classList.add(TopBar.CLASS.COVER);
 
-        let cover = document.createElement("div");
-        cover.className = BASE_CLASS_NAME + "_cover";
-
-        let topMenu = document.createElement("div");
-        topMenu.className = BASE_CLASS_NAME + "_top_menu";
+        this.#elements.topMenu = document.createElement("div");
+        this.#elements.topMenu.classList.add(TopBar.CLASS.TOP_MENU);
 
         let home = this.getHome();
         if(home != null){
-            let homeBtn = document.createElement("img");
-            homeBtn.src =  this.getHomeIcon();
-            homeBtn.classList.add(topMenu.classList[0] + "_home");
-            homeBtn.onclick = function(){
+            this.#elements.homeBtn = document.createElement("img");
+            this.#elements.homeBtn.classList.add(TopBar.CLASS.HOME_BUTTON);
+            this.#elements.homeBtn.src =  this.getHomeIcon();
+            this.#elements.homeBtn.onclick = function(){
                 location.href = home;
             };
 
-            topMenu.appendChild(homeBtn);
+            this.#elements.topMenu.appendChild(this.#elements.homeBtn);
         }
 
-        let blank = document.createElement("div");
-        blank.className = BASE_CLASS_NAME + "_blank";
+        this.#elements.blank = document.createElement("div");
+        this.#elements.blank.classList.add(TopBar.CLASS.BLANK);
 
-        let titleBar = document.createElement("p");
-        titleBar.innerText = this.getTitle();
-        titleBar.className = BASE_CLASS_NAME + "_title";
+        this.#elements.titleBar = document.createElement("p");
+        this.#elements.titleBar.classList.add(TopBar.CLASS.TITLEBAR);
+        this.#elements.titleBar.innerText = this.getTitle();
 
-        blank.appendChild(titleBar);
+        this.#elements.blank.appendChild(this.#elements.titleBar);
 
-        let bottomMenu = document.createElement("div");
-        bottomMenu.className = BASE_CLASS_NAME + "_bottom_menu";
+        this.#elements.bottomMenu = document.createElement("div");
+        this.#elements.bottomMenu.classList.add(TopBar.CLASS.BOTTOM_MENU);
 
-        let bottomMenuBtns = [];
-        for(let i = 0; i < 4; i++){
+        this.#elements.bottomMenuBtns = [];
+        for(let i = 0; i < this.#bottomMenuItems.length; i++){
             let btn = document.createElement("div");
             let label = document.createElement("div");
             let labelText = document.createElement("p");
             let icon = document.createElement("img");
 
-            btn.className = bottomMenu.className + "_button";
+            btn.className = TopBar.CLASS.BOTTOM_MENU_BUTTON;
             btn.onclick = function(){
-                window.open(urls[i][1]);
+                window.open(this.#bottomMenuItems[i].url);
             };
 
-            label.classList.add(btn.classList[0]+ "_label");
+            label.classList.add(TopBar.CLASS.BOTTOM_MENU_BUTTON_LABEL);
 
-            labelText.classList.add(btn.classList[0]+ "_label_text");
-            labelText.innerText = urls[i][0];
+            labelText.classList.add(TopBar.CLASS.BOTTOM_MENU_BUTTON_LABEL_TEXT);
+            labelText.innerText = this.#bottomMenuItems[i].text;
 
-            icon.classList.add(btn.classList[0]+ "_label_icon");
-            icon.src = urls[i][2];
+            icon.classList.add(TopBar.CLASS.BOTTOM_MENU_BUTTON_LABEL_ICON);
+            icon.src = this.#bottomMenuItems[i].icon;
 
             label.appendChild(icon);
             label.appendChild(labelText);
             btn.appendChild(label);
-            bottomMenu.appendChild(btn);
+            this.#elements.bottomMenu.appendChild(btn);
+
+            this.#elements.bottomMenuBtns.push({
+                btn: btn,
+                label: label,
+                labelText: labelText,
+                icon: icon,
+            });
         }
 
-        cover.appendChild(topMenu);
-        cover.appendChild(blank);
-        cover.appendChild(bottomMenu);
+        this.#elements.cover.appendChild(this.#elements.topMenu);
+        this.#elements.cover.appendChild(this.#elements.blank);
+        this.#elements.cover.appendChild(this.#elements.bottomMenu);
     
-        this.#back.appendChild(cover);
+        this.#elements.back.appendChild(this.#elements.cover);
     }
 
     #load(){
-        this.#back.style.background = this.#background;
+        this.#elements.back.style.background = this.#background;
+        this.renew();
     }
 
     build(){
         this.#load();
-        return this.#back;
+        return this.#elements.back;
     }
 
     setTitle(title){
@@ -123,19 +176,19 @@ class TopBar {
     }
 
     setBackground(background){
-        this.#background = (background.charAt(0) == "#") ? background : `url("${background}")`;
+        this.#elements.background = (background.charAt(0) == "#") ? background : `url("${background}")`;
     }
 
     getBackground(){
-        return (this.#background.charAt(0) == "#") ? this.#background : this.#background.slice(5, -2); 
+        return (this.#elements.background.charAt(0) == "#") ? this.#elements.background : this.#elements.background.slice(5, -2); 
     }
 
     getHomeIcon(){
-        return this.#HOME_ICON;
+        return this.#homeIcon;
     }
 
     setHomeIcon(path){
-        this.#HOME_ICON = path;
+        this.#homeIcon = path;
     }
 
     getHome(){
@@ -145,15 +198,26 @@ class TopBar {
     setHome(path){
         this.#home = path;
     }
+
+    setMenu(arr){
+        this.#bottomMenuItems = arr;
+    }
+
+    addMenu(item){
+        this.#bottomMenuItems.push(item);
+    }
+
+    getMenu(){
+        return this.#bottomMenuItems;
+    }
 }
 
 var TopBars = [];
-var elements = document.getElementsByClassName(BASE_CLASS_NAME);
+var elements = document.getElementsByClassName(TopBar.CLASS.BASE);
 for(let i = 0; i < elements.length; i++){
     let topbar = new TopBar();
     topbar.setHome("https://pages.kurosaki.love");
-    
-    topbar.renew();
+    topbar.setMenu(menuItems);
 
     elements[i].appendChild(topbar.build());
     TopBars.push(topbar);
