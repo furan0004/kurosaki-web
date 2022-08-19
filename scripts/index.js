7a2aa59623a09547c3b936f2d63d4c71dd86ae68
K <furan0004@gmail.com>
2022-08-19 19:04:35 +0900

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,14 @@
-export default 0;
-
 import { TopBar } from "./topbar.js";
 import menuItems  from "../data/menuItems.json" assert { type: "json" };
 import indexSheet from "../styles/index.css" assert {type: "css"};
 document.adoptedStyleSheets.push(indexSheet);
 
 
-var TopBars = [];
 var elements = document.getElementsByClassName(TopBar.CLASS.BASE);
-for(let i = 0; i < elements.length; i++){
-    let topbar = new TopBar();
-    topbar.setHome("https://pages.kurosaki.love");
-    topbar.setMenu(menuItems);
 
-    elements[i].appendChild(topbar.build());
-    TopBars.push(topbar);
-}
+export var topbar;
+topbar = new TopBar();
+topbar.setHome("https://pages.kurosaki.love");
+topbar.setMenu(menuItems);
+
+elements[0].appendChild(topbar.build());
