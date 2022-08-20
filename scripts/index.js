c3dcb71b4442d7ab12b01fec32035865f6637730
K <furan0004@gmail.com>
2022-08-20 15:47:50 +0900

    test query

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,6 @@ import getQuery from "./queryString.js";
 import indexSheet from "../styles/index.css" assert {type: "css"};
 document.adoptedStyleSheets.push(indexSheet);
 
-
 var elements = document.getElementsByClassName(TopBar.CLASS.BASE);
 
 var topbar;
@@ -14,5 +13,6 @@ topbar.setHome("https://pages.kurosaki.love");
 topbar.setMenu(menuItems);
 
 elements[0].appendChild(topbar.build());
-
 export default topbar;
+
+console.log(getQuery(location.href));
