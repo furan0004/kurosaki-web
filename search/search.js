463f765ec41f694c8bf830f6311e4f4d8cc8a161
K <furan0004@gmail.com>
2022-08-20 17:54:31 +0900

    fix test search 2

--- search/search.js
+++ search/search.js
@@ -1,12 +1,25 @@
 import topbar from "../scripts/index.js";
 import { getQuery } from "../scripts/queryString.js";
+import pageData from "../data/pages.json";
+
+var line = document.getElementsByClassName("line")[0];
 
 var query = getQuery(location.href);
 var queries = decodeURIComponent(query["q"]).replaceAll("　", " ").split(" ");
-console.log(queries);
 
-topbar.setTitle(document.title);
-topbar.load();
-topbar.setSearchQuery(queries);
+(function(){
+    topbar.setTitle(document.title);
+    topbar.setMenu([]);
+    topbar.load();
+    topbar.setSearchQuery(queries);
 
-var line = document.getElementsByClassName("line")[0];
+    for(let i = 0; i < pageData.length; i++){
+        let p = document.createElement("p");
+        let keys = Object.keys(pageData[i]);
+        for(let j = 0; j < keys.length; j++){
+            p.innerHTML += `${(i == 0 ? "" : "<br>")} ${keys[i]}: ${pageData[keys[i]]}`;
+        }
+
+        line.appendChild(p);
+    }
+})();
