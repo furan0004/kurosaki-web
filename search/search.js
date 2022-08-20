cd00fc115963f373528a898dd09adbdf883bf2d1
K <furan0004@gmail.com>
2022-08-20 19:06:31 +0900

    fix test search 3

--- search/search.js
+++ search/search.js
@@ -20,18 +20,35 @@ var queries = decodeURIComponent(query["q"]).replaceAll("　", " ");
     for(let i = 0; i < pageData.length; i++){
         let item = createItem(pageData[i]);
 
-        let p = document.createElement("p");
-        let keys = Object.keys(pageData[i]);
-        for(let j = 0; j < keys.length; j++){
-            p.innerHTML += `${(j == 0 ? "" : "<br>")} ${keys[j]}: ${pageData[i][keys[j]]}`;
-        }
-
-        line.appendChild(p);
+        line.appendChild(item);
     }
 })();
 
 function createItem(info){
     let back = document.createElement("div");
+
+    let urlRow = document.createElement("div");
+    urlRow.classList.add("item-row");
+    urlRow.classList.add("item-url");
+    urlRow.innerText = restrictLength(info.url, 20, "...");
+
+    let titleRow = document.createElement("div");
+    titleRow.classList.add("item-row");
+
+    let anchor = document.createElement("a");
+    anchor.classList.add("item-title");
+
+    anchor.innerText = restrictLength(info.title, 20, "...");
+    titleRow.appendChild(anchor);
+
+    let descriptionRow = document.createElement("div");
+    descriptionRow.classList.add("item-row");
+    descriptionRow.classList.add("item-description");
+    descriptionRow.innerText = restrictLength(info.description.replaceAll("\n", ""), 200, "...");
+
+    back.appendChild(urlRow);
+    back.appendChild(titleRow);
+    back.appendChild(descriptionRow);
 
     return back;
 }
