5885fb2d27a1aa5735e0895e92a7b207950a325b
K <furan0004@gmail.com>
2022-08-24 06:55:38 +0900

    fix

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,9 @@ import itemStyle from "../styles/elementItems.css" assert {type: "css"};
 document.adoptedStyleSheets.push(itemStyle);
 
 var topbar = new TopBar();
-var elements = document.getElementsByClassName(TopBar.CLASS.BASE);
-elements[0].appendChild(topbar.build());
+
+(function(){
+    document.getElementsByClassName(TopBar.CLASS.BASE)[0].appendChild(topbar.build());
+})();
 
 export default topbar;
